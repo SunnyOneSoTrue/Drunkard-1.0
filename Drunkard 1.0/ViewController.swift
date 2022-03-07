0e5d886ca08860ec2ca9cb95b21e89deaea01f15
SunnyOneSoTrue <kakhianisandro12@gmail.com>
2022-03-07 13:07:28 +0400

    Initial Commit

---
+++ Drunkard 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drunkard 1.0
+//
+//  Created by USER on 07.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
