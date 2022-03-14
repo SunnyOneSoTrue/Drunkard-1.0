5b73fa148c4b5b67e7e7fa3d133d08ef5538f5c1
SunnyOneSoTrue <kakhianisandro12@gmail.com>
2022-03-14 14:24:40 +0400

    added Sign In logic

--- Drunkard 1.0/ViewControllers/Authentification /SignInViewController.swift
+++ Drunkard 1.0/ViewControllers/Authentification /SignInViewController.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField?
+    @IBOutlet weak var passwordField: UITextField?
+    @IBOutlet weak var warningLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        warningLabel.isHidden = true
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onSignIn(_ sender: UIButton) {
+        
+        guard let email = emailField?.text! else {return}
+        guard let password = passwordField?.text! else {return}
+        
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {[weak self] result, error in
+            
+            guard let strongSelf = self else { return }
+            
+            guard error == nil else {
+                strongSelf.warningLabel.text = "\(error!.localizedDescription)"
+                strongSelf.warningLabel.isHidden = false
+                
+                if let errCode = AuthErrorCode(rawValue: error!._code) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                    switch errCode {
+                    case .invalidEmail, .missingEmail:
+                        self?.emailField?.layer.borderColor = CGColor.init(red: 1, green: 0, blue: 0, alpha: 1)
+                    case .wrongPassword:
+                        self?.passwordField?.layer.borderColor = CGColor.init(red: 1, green: 0, blue: 0, alpha: 1)
+                    default:
+                        print("Create User Error: \(error!)")
+                    }
+                }
+                
+                
+                return
+            }
+            print("you have signed in")
+            //TODO: Send to app
+        })
     }
-    */
-
+    
+    @IBAction func onSignUp(_ sender: UIButton) {
+    }
 }
